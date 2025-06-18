7c618a6c46a1f271697af9a041c0f6d2a11edbdd
notabird <notabird07@gmail.com>
2025-06-18 04:18:04 -0700

    Initial commit

---
+++ CoolerDoug/Plugin Info.cs
@@ -0,0 +1,9 @@
+namespace CoolerDoug
+{
+    public class Plugin_Info
+    {
+        public const string GUID = "NotABird.Cooler.Doug",
+                            NAME = "Cooler Doug",
+                            VERSION = "1.0.0";
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This mod uses GameObject.Find on an object that is likly to change in the next few updates so it will prob break soon
